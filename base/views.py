09c70323ddef67123c72e8a1919c0aceffb40a86
faizan <faizanahmed14887@gmail.com>
2021-05-05 09:52:22 -0700

    initial commit

---
+++ base/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import EmployeeSerializer
+# Create your views here.
+
+@api_view(['POST'])
+def employeeCreate(request):
+    serializer = EmployeeSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
