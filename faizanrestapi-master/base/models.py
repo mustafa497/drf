1655e6aac594f252fc3bef232226b1b5e931e1d2
mustafa <mustafaadil539@gmail.com>
2021-05-08 23:07:22 +0500

    first commit

---
+++ faizanrestapi-master/base/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Employee(models.Model):
+    number = models.CharField(max_length=200, null=True, blank=True)
+    optin = models.BooleanField(default=False)
+    userid = models.CharField(max_length=200, null=True, blank=True)
+
+    def __str__(self, *args, **kwargs):
+        return self.userid
