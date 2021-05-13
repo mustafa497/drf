d31c4cdebf21f345c78c99c3876e1b8fa5dbd8f3
mustafa497 <57321038+mustafa497@users.noreply.github.com>
2021-05-13 19:21:54 +0500

    Update urls.py

--- faizanrestapi-master/base/urls.py
+++ faizanrestapi-master/base/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from . import views
 
+
+
 urlpatterns = [
-    path('create/', views.employeeCreate, name='employee-create'),
-]
+    path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/profile/', views.getuserProfile, name='users-profile'),
+
+    path('employee/create/', views.employeeCreate, name='employee-create'),
+    path('employees/', views.getEmployees, name='employees'),
+    path('employee/<str:pk>/', views.getEmployee, name='employee'),
+]    
